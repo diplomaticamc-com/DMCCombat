23bc746bebc346fd8f5f9112881e9cc5969e8017
georgbjarn <81572612+GeorgBjarn@users.noreply.github.com>
2025-07-28 23:38:54 +0000

    added config options for spawn protection and newbie protection

--- src/main/java/net/earthmc/emccom/config/Config.java
+++ src/main/java/net/earthmc/emccom/config/Config.java
@@ -8,6 +8,16 @@ public class Config {
         config.addDefault("ender_pearl_cooldown_ticks", 240);
         config.addDefault("golden_apple_cooldown_ticks",100);
         config.addDefault("riptide_in_combat_cooldown_ticks",400);
+        config.addDefault("protection-time-minutes", 720);
+        config.addDefault("protection-message", "&aYou are under newbie protection for %minutes% more minutes!");
+        config.addDefault("attack-blocked-message", "&cYou cannot fight while protected!");
+        config.addDefault("protection-ended-message", "&eYour newbie protection has ended");
+
+        config.addDefault("reload-message", "&aConfiguration reloaded");
+
+        config.addDefault("spawn_protection.use_chunks", false);
+        config.addDefault("spawn_protection.chunks_amount", 32);
+        config.addDefault("spawn_protection.blocks_amount", 32);
 
         config.options().copyDefaults(true);
     }
